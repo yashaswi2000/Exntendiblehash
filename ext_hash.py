9e5c26f03aa0ee318f85d6cf9541a040cdc2f763
yashaswi <2017csb1089@iitrpr.ac.in>
2021-02-15 23:38:20 +0530

    commit 4

--- ext_hash.py
+++ ext_hash.py
@@ -4,9 +4,29 @@ from ssm import *
 import pandas as pd
 
 
-def directory_expansion(dirarray,global_depth):
+def insert_directory(simobject,dir_bucket,dirobject):
+    if dir_bucket.link == -1 and dir_bucket.empty_spaces > 0:
+        dir_bucket.array[simobject.d_size - dir_bucket.empty_spaces] = dirobject
+        dir_bucket.empty_spaces -= 1
+    elif dir_bucket.link == -1 and dir_bucket.empty_spaces == 0:
+        directory_bucket = simobject.new_direct_bucket()
+        directory_bucket.array[simobject.d_size - directory_bucket.empty_spaces] = dirobject
+        directory_bucket.empty_spaces -= 1
+        simobject.bucket_array[simobject.current_overflow] = directory_bucket
+        dir_bucket.link = simobject.current_overflow
+        dir_bucket.last = 0
+        simobject.clean_overflow()
+    else:
+        insert_directory(simobject,simobject.bucket_array[dir_bucket.link],dirobject)
+        
+def clean_directory(simobject,od_pointer):
     pass
-    if 2 * len(dirarray) <= 1024:
+    
+
+
+def directory_expansion(dirarray,global_depth,simobject,od_pointer,dir_length):
+    pass
+    if 2 * len(dirarray) <= dir_length:
         pass
         l = len(dirarray)
         for i in range(l):
@@ -14,11 +34,44 @@ def directory_expansion(dirarray,global_depth):
             dirarray.pop(0)
             dirarray.append(directory(temp_dir.hash_prefix<<1,temp_dir.pointer))
             dirarray.append(directory((temp_dir.hash_prefix<<1) + 1,temp_dir.pointer))
-        global_depth += 1
-        return global_depth
     else:
         pass
-        l = 1024
+        l = len(dirarray)
+        if od_pointer != None:
+            directory_bucket = simobject.bucket_array[od_pointer]
+            while directory_bucket.link != -1:
+                l += simobject.d_size - directory_bucket.empty_spaces
+            
+            for i in range(l):
+                temp_dir = dirarray[0]
+                dirarray.pop(0)
+                insert_directory(simobject, simobject.bucket_array[od_pointer], directory(temp_dir.hash_prefix<<1,temp_dir.pointer))
+                insert_directory(simobject, simobject.bucket_array[od_pointer], directory((temp_dir.hash_prefix<<1) + 1,temp_dir.pointer))
+                dirarray.append(clean_directory(simobject,od_pointer))     
+        
+        else:
+            for i in range(l):
+                temp_l = len(dirarray)
+                if temp_l + 1 <= dir_length:
+                    temp_dir = dirarray[0]
+                    dirarray.pop(0)
+                    dirarray.append(directory(temp_dir.hash_prefix<<1,temp_dir.pointer))
+                    dirarray.append(directory((temp_dir.hash_prefix<<1) + 1,temp_dir.pointer))
+                else:
+                    temp_dir = dirarray[0]
+                    dirarray.pop(0)
+                    if od_pointer == None:
+                        simobject.bucket_array[simobject.current_overflow] = simobject.new_direct_bucket()
+                        od_pointer = simobject.current_overflow
+                        simobject.clean_overflow()
+                    insert_directory(simobject, simobject.bucket_array[od_pointer], directory(temp_dir.hash_prefix<<1,temp_dir.pointer))
+                    insert_directory(simobject, simobject.bucket_array[od_pointer], directory((temp_dir.hash_prefix<<1) + 1,temp_dir.pointer))
+                    dirarray.append(clean_directory(simobject,od_pointer))  
+                    
+    global_depth += 1
+    return global_depth
+        
+        
 
 def get_records_overflow(simobject,records_array,record_bucket):
     pass
@@ -30,9 +83,9 @@ def get_records_overflow(simobject,records_array,record_bucket):
     return None    
     
     
-def splitting(dirarray, simobject, index, global_depth, bucket_rc, flag):
+def splitting(dirarray, simobject, index, global_depth, bucket_rc, flag,dir_length):
     pass
-    if len(dirarray) <= 1024:
+    if len(dirarray) <= dir_length:
         target = dirarray[index].pointer
         #record variables
         record_bucket = simobject.bucket_array[target]
@@ -63,7 +116,7 @@ def splitting(dirarray, simobject, index, global_depth, bucket_rc, flag):
     
 
 
-def insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag):
+def insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag,od_pointer,dir_length):
     pass
     to_hash = record.id
     hash_value = hash(to_hash)
@@ -74,7 +127,7 @@ def insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag):
     else:
         index = int(index[0:global_depth],2)
     print(index,"this is it")
-    if index < 1024:
+    if index < dir_length:
         print(dirarray[index])
         if dirarray[index] == None:
             pass
@@ -97,15 +150,15 @@ def insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag):
             if simobject.bucket_array[dirarray[index].pointer].depth == global_depth and flag==0 and simobject.bucket_array[dirarray[index].pointer].link==-1:
                 pass
                 print("gd == ld")
-                global_depth = directory_expansion(dirarray,global_depth)
+                global_depth = directory_expansion(dirarray,global_depth,simobject,od_pointer,dir_length)
                 visualise(dirarray,simobject)
-                global_depth = insert_record(simobject,record,dirarray,global_depth,bucket_rc,1)
+                global_depth = insert_record(simobject,record,dirarray,global_depth,bucket_rc,1,od_pointer,dir_length)
             
             elif simobject.bucket_array[dirarray[index].pointer].depth < global_depth:
                 pass
                 print("spltting")
                 global_depth = splitting(dirarray, simobject, index, global_depth, bucket_rc, flag)
-                global_depth = insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag)
+                global_depth = insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag,od_pointer,dir_length)
             
             else:
                 print("overflow")
@@ -131,9 +184,9 @@ def insert_record(simobject,record,dirarray,global_depth,bucket_rc,flag):
                         record_bucket.empty_spaces -= 1
                     elif flag!=1:
                         pass
-                        global_depth = directory_expansion(dirarray,global_depth)
+                        global_depth = directory_expansion(dirarray,global_depth,simobject,od_pointer,dir_length)
                         visualise(dirarray,simobject)
-                        global_depth = insert_record(simobject,record,dirarray,global_depth,bucket_rc,1)
+                        global_depth = insert_record(simobject,record,dirarray,global_depth,bucket_rc,1,od_pointer,dir_length)
                     else:
                         overflow_bucket = simobject.new_record_bucket(0)
                         overflow_bucket.array[bucket_rc - overflow_bucket.empty_spaces] = record
@@ -181,13 +234,13 @@ data = pd.read_csv("dataset.csv")
 dirarray = [None]
 secondary_mem = sim_secondary_mem(1000,3,100)
 global_depth = 0
-dir_length = 1
+dir_length = 1024
 od_pointer = None
 
 for d in data.iterrows():
     recordobj = records(d[1][0],d[1][1],d[1][2],d[1][3])
     #print(recordobj)
-    global_depth = insert_record(secondary_mem,recordobj,dirarray,global_depth,3,0)
+    global_depth = insert_record(secondary_mem,recordobj,dirarray,global_depth,3,0,od_pointer,dir_length)
     visualise(dirarray,secondary_mem)
     print("--------------------------")
     
